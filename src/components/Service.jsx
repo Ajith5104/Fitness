3b80e15450c67bafb4bc8459f2fb944f41d4f55e
Ajith5104 <ajithkumar51046@gmail.com>
2025-01-17 17:48:46 +0530

    first commit

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 // Importing images for services
-import Img1 from '../assets/Physical-Fitness.jpg';
-import Img2 from '../assets/Weight-Gain.jpg';
-import strengthImage from '../assets/straining.jpg'; 
-import Img4 from '../assets/Fat-Loss.jpg';
-import Img5 from '../assets/Weight-lifting.jpg';
-import Img6 from '../assets/Running.jpg';
+import PhysicalFitness from '../assets/PhysicalFitness.jpg';
+import WeightGain from '../assets/WeightGain.jpg';
+import Strength from '../assets/strength.jpg'; 
+import FatLoss from '../assets/FatLoss.jpg';
+import Weightlifting from '../assets/Weightlifting.jpg';
+import Running from '../assets/Running.jpg';
 
 const Service = () => {
   return (
@@ -20,37 +20,37 @@ const Service = () => {
       <div className="services-content" data-aos="zoom-in-up">
         {/* Service Row: Physical Fitness */}
         <div className="rows">
-          <img src={Img1} alt="Physical Fitness" />
+          <img src={PhysicalFitness} alt="Physical Fitness" />
           <h4>Physical Fitness</h4>
         </div>
 
         {/* Service Row: Weight Gain */}
         <div className="rows">
-          <img src={Img2} alt="Weight Gain" />
+          <img src={WeightGain} alt="Weight Gain" />
           <h4>Weight Gain</h4>
         </div>
 
         {/* Service Row: Strength Training */}
         <div className="rows">
-          <img src={strengthImage} alt="Strength Training" />
+          <img src={Strength} alt="Strength Training" />
           <h4>Strength Training</h4>
         </div>
 
         {/* Service Row: Fat Loss */}
         <div className="rows">
-          <img src={Img4} alt="Fat Loss" />
+          <img src={FatLoss} alt="Fat Loss" />
           <h4>Fat Loss</h4>
         </div>
 
         {/* Service Row: Weight Lifting */}
         <div className="rows">
-          <img src={Img5} alt="Weightlifting" />
+          <img src={Weightlifting} alt="Weightlifting" />
           <h4>Weight Lifting</h4>
         </div>
 
         {/* Service Row: Running */}
         <div className="rows">
-          <img src={Img6} alt="Running" />
+          <img src={Running} alt="Running" />
           <h4>Running</h4>
         </div>
       </div>
